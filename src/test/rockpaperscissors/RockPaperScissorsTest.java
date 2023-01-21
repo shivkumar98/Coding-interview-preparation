94c9069bca7470752a0f3baa69e1ff8cf81258a7
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2023-01-21 13:25:19 +0000

    Added more unit test

--- src/test/rockpaperscissors/RockPaperScissorsTest.java
+++ src/test/rockpaperscissors/RockPaperScissorsTest.java
@@ -6,8 +6,29 @@ import static org.junit.Assert.assertEquals;
 
 public class RockPaperScissorsTest {
     @Test
-    public void arbitraryTest(){
-        assertEquals(1,1);
+    public void arbitraryTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void nezukoWinsOneRound(){
+        int[] result = RockPaperScissors.rockPaperScissor(1, "R", "S");
+        assertEquals(1,result[0]);
+        assertEquals(0,result[1]);
+    }
+
+    @Test
+    public void zenitsuWinsOneRound(){
+        int[] result = RockPaperScissors.rockPaperScissor(1, "S", "R");
+        assertEquals(0, result[0]);
+        assertEquals(1, result[1]);
+    }
+
+    @Test
+    public void testThatNezukosPlayCyclesBack(){
+        int[] result = RockPaperScissors.rockPaperScissor(3, "SP", "RRR");
+        assertEquals(1,result[0]);
+        assertEquals(2,result[1]);
     }
 }
 
